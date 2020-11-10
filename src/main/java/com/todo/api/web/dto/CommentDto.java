25c4de1938a92e4a1d2bb9232bdf6e32e07c520e
shoon2430 <shoon2430@gmail.com>
2020-11-10 21:38:17 +0900

    작성일로부터 지난시간을 계산하도록 수정

--- src/main/java/com/todo/api/web/dto/CommentDto.java
+++ src/main/java/com/todo/api/web/dto/CommentDto.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +23,7 @@ public class CommentDto {
     private String text;        // 내용
     private String writer;      // 작성자ID
     private String likePoint;   // 좋아요
+    private String created;
     private List<SubCommentDto> subComments = new ArrayList<SubCommentDto>();
 
     public CommentDto(Comment comment) {
@@ -29,6 +32,21 @@ public class CommentDto {
         this.writer = comment.getWriter();
         this.likePoint = comment.getLikePoint();
         this.todoId = comment.getTodo().getId();
+        this.created = getBeforeDay(comment.getCreated());
         this.subComments = comment.getSubComments().stream().map(subComment -> new SubCommentDto(subComment)).collect(Collectors.toList());
+    }
+
+    private String getBeforeDay(LocalDateTime date) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        Duration duration = Duration.between(date, currentDateTime);
+        long second = duration.getSeconds();
+        if (60 > second) return second + " 초 전";
+        if (60 * 60 > second) return second / 60 + " 분 전";
+        if (60 * 60 * 24 > second) return second / (60 * 60) + " 시간 전";
+        if (60 * 60 * 24 * 30 > second) return second / (60 * 60 * 24) + " 일 전";
+        if (60 * 60 * 24 * 365> second) return second / (60 * 60 * 24 * 30) + " 달 전";
+
+        else return second / (60 * 60 * 24 * 365) + " 년 전";
     }
 }
